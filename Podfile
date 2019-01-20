83f1ebab371c625bc8b25e1803969cde88846a0b
Luca Scaringella <lucalstyle@gmail.com>
2019-01-20 16:40:07 +0100

    init project

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'zation-swift-client' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for zation-swift-client
+
+  target 'zation-swift-clientTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ zation-swift-client/zation_swift_client.h
@@ -0,0 +1,19 @@
+//
+//  zation_swift_client.h
+//  zation-swift-client
+//
+//  Created by Luca Scaringella on 20.01.19.
+//  Copyright © 2019 Luca Scaringella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for zation_swift_client.
+FOUNDATION_EXPORT double zation_swift_clientVersionNumber;
+
+//! Project version string for zation_swift_client.
+FOUNDATION_EXPORT const unsigned char zation_swift_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zation_swift_client/PublicHeader.h>
+
+
